2de6d3a66916dec17261287bc651ce4fc77a7eb2
andirh <57002449+andirh@users.noreply.github.com>
2021-05-10 16:57:45 +0200

    Made variables final

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -3,8 +3,8 @@ import java.lang.*;
 import java.util.*;
 
 class Customer {
-    private String name;
-    private Vector rentals = new Vector();
+    private final String name;
+    private final Vector rentals = new Vector();
     public Customer (String newname){
         name = newname;
     };
@@ -17,7 +17,7 @@ class Customer {
     public String statement() {
         double totalAmount = 0;
         int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
